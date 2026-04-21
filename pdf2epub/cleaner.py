3bff1d368a81ea9f649d5ff91a9f59b230831c7a
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-04-21 12:59:06 +0200

    Fix Persian cleaning to reverse word order, not character order

--- pdf2epub/cleaner.py
+++ pdf2epub/cleaner.py
@@ -5,14 +5,27 @@ def fix_persian_text(text):
     if not text:
         return text
 
-    tokens = re.findall(r'\S+|\s+', text)
-    fixed = []
-    for token in tokens:
-        if token.isspace():
-            fixed.append(token)
-        else:
-            fixed.append(token[::-1])
-    return ''.join(fixed)
+    fixed_lines = []
+    for line in text.splitlines(keepends=True):
+        tokens = re.findall(r'\s+|\S+', line)
+        words = [t for t in tokens if not t.isspace()]
+        if len(words) < 2:
+            fixed_lines.append(line)
+            continue
+
+        reversed_words = list(reversed(words))
+        fixed_line = []
+        word_index = 0
+        for token in tokens:
+            if token.isspace():
+                fixed_line.append(token)
+            else:
+                fixed_line.append(reversed_words[word_index])
+                word_index += 1
+
+        fixed_lines.append(''.join(fixed_line))
+
+    return ''.join(fixed_lines)
 
 
 def split_paragraphs(text):
